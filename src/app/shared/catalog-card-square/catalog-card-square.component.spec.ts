e8060813e09e27ba8064486bae76095df13bc0b0
Komiljon <komiljonnajmitdinov99@gmail.com>
2022-03-09 18:03:30 +0500

    add component

--- src/app/shared/catalog-card-square/catalog-card-square.component.spec.ts
+++ src/app/shared/catalog-card-square/catalog-card-square.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SliderTrucksSquareComponent } from './slider-trucks-square.component';
+import { CatalogCardSquareComponent } from './catalog-card-square.component';
 
-describe('SliderTrucksSquareComponent', () => {
-  let component: SliderTrucksSquareComponent;
-  let fixture: ComponentFixture<SliderTrucksSquareComponent>;
+describe('CatalogCardSquareComponent', () => {
+  let component: CatalogCardSquareComponent;
+  let fixture: ComponentFixture<CatalogCardSquareComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SliderTrucksSquareComponent ]
+      declarations: [ CatalogCardSquareComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SliderTrucksSquareComponent);
+    fixture = TestBed.createComponent(CatalogCardSquareComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/shared/catalog-card-square/catalog-card-square.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-card-square',
+  templateUrl: './catalog-card-square.component.html',
+  styleUrls: ['./catalog-card-square.component.scss']
+})
+export class CatalogCardSquareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
